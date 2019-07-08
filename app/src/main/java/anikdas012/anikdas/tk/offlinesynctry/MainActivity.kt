508d34827a01e08016c1501f9a95e4c014eb5375
Anik Das <anikdas012@gmail.com>
2019-07-08 16:00:54 +0600

    Initial commit

---
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/MainActivity.kt
@@ -0,0 +1,12 @@
+package anikdas012.anikdas.tk.offlinesynctry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
