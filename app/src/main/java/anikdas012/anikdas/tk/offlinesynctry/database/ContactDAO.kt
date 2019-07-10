0f332a49230dfce2f4b8e2e72642ab20e4f8c163
Anik Das <anikdas012@gmail.com>
2019-07-10 16:39:20 +0600

    Added necessary methods to DAO

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
@@ -1,7 +1,21 @@
 package anikdas012.anikdas.tk.offlinesynctry.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface ContactDAO {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun createNewContact(contact: Contact)
+
+    @Query("DELETE FROM Contacts")
+    fun deleteAll()
+
+    @Query("SELECT * from Contacts ORDER BY id ASC")
+    fun getAll(): List<Contact>
+
+    @Query("SELECT * from Contacts WHERE status = 1 ORDER BY id ASC")
+    fun getAllUnsynced(): List<Contact>
 }
