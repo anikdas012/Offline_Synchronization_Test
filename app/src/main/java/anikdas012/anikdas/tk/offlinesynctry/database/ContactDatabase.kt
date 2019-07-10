7f37d5679d137227f4450d3ec004101def3480c2
Anik Das <anikdas012@gmail.com>
2019-07-10 17:16:39 +0600

    Changed class annotation

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Contact::class], version = 1)
+@Database(entities = arrayOf(Contact::class), version = 1)
 abstract class ContactDatabase: RoomDatabase() {
 
     abstract fun contactDao(): ContactDAO
