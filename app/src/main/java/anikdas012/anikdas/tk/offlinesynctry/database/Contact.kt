329b502f36b12fdd931714c98302def884db36b6
Anik Das <anikdas012@gmail.com>
2019-07-10 15:05:53 +0600

    Completed the entity class

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/Contact.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/Contact.kt
@@ -1,10 +1,13 @@
 package anikdas012.anikdas.tk.offlinesynctry.database
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "Contacts")
 data class Contact(
-    @PrimaryKey(autoGenerate = true) var id: Int,
-    @ColumnInfo(name = "Name") var name: String,
-    @ColumnInfo(name = "Phone_Number") var number: String
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "Name") val name: String,
+    @ColumnInfo(name = "Phone_Number") val number: String,
+    val status: Int
 )
