777fe19dafbb999006e994a2256d598940804aa7
Anik Das <anikdas012@gmail.com>
2019-07-10 16:58:21 +0600

    Added method to get DAO object

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
@@ -5,4 +5,5 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Contact::class], version = 1)
 abstract class ContactDatabase: RoomDatabase() {
+    abstract fun contactDao(): ContactDAO
 }
