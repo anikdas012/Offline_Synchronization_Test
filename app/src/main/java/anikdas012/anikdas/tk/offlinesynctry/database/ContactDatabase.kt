a26ae6a3d08badb51d5b2940f96d320560cc680a
Anik Das <anikdas012@gmail.com>
2019-07-10 16:57:12 +0600

    Annotated the class a database

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
@@ -1,6 +1,8 @@
 package anikdas012.anikdas.tk.offlinesynctry.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(entities = [Contact::class], version = 1)
 abstract class ContactDatabase: RoomDatabase() {
 }
