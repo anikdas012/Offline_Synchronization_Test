af29325fa2549ba79a7142e928c54aae76eb015b
Anik Das <anikdas012@gmail.com>
2019-07-10 18:28:06 +0600

    Added a new class named AppUtil and two new class property

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/AppUtil.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/AppUtil.kt
@@ -1,2 +1,8 @@
 package anikdas012.anikdas.tk.offlinesynctry.util
 
+class AppUtil {
+    companion object {
+        final val STATUS_SYNCED: Int = 0
+        final val STATUS_UNSYNCED: Int = 1
+    }
+}
