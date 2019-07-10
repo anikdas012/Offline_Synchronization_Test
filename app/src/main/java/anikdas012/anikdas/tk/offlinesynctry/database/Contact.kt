b472679543bd18e42c4f43aaa472cbed473fca87
Anik Das <anikdas012@gmail.com>
2019-07-10 15:04:21 +0600

    Added properties to primary constructor

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/Contact.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/Contact.kt
@@ -1,10 +1,10 @@
-package anikdas012.anikdas.tk.offlinesynctry.model
+package anikdas012.anikdas.tk.offlinesynctry.database
 
 import androidx.room.ColumnInfo
 import androidx.room.PrimaryKey
 
 data class Contact(
     @PrimaryKey(autoGenerate = true) var id: Int,
-    @ColumnInfo() var name: String,
-    var number: String
+    @ColumnInfo(name = "Name") var name: String,
+    @ColumnInfo(name = "Phone_Number") var number: String
 )
