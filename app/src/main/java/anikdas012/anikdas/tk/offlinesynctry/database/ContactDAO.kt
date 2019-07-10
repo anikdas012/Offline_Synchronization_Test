05a3d32c206e92510e3781ccb73b1451fc4e354e
Anik Das <anikdas012@gmail.com>
2019-07-10 16:51:51 +0600

    Changed to LiveData instead of List

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
@@ -1,5 +1,6 @@
 package anikdas012.anikdas.tk.offlinesynctry.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -14,8 +15,8 @@ interface ContactDAO {
     fun deleteAll()
 
     @Query("SELECT * from Contacts ORDER BY id ASC")
-    fun getAll(): List<Contact>
+    fun getAll(): LiveData<List<Contact>>
 
     @Query("SELECT * from Contacts WHERE status = 1 ORDER BY id ASC")
-    fun getAllUnsynced(): List<Contact>
+    fun getAllUnsynced(): LiveData<List<Contact>>
 }
