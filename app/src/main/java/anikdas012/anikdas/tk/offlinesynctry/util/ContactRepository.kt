13b2f99a5c927dbe982e115e999468a2d613b27b
Anik Das <anikdas012@gmail.com>
2019-07-10 18:06:49 +0600

    Created a suspend method to add new contact

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
@@ -1,5 +1,6 @@
 package anikdas012.anikdas.tk.offlinesynctry.util
 
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import anikdas012.anikdas.tk.offlinesynctry.database.Contact
 import anikdas012.anikdas.tk.offlinesynctry.database.ContactDAO
@@ -7,4 +8,9 @@ import anikdas012.anikdas.tk.offlinesynctry.database.ContactDAO
 class ContactRepository(private val contactDao: ContactDAO) {
 
     val allContacts: LiveData<List<Contact>> = contactDao.getAll()
+
+    @WorkerThread
+    suspend fun createContact(contact: Contact) {
+        contactDao.createNewContact(contact)
+    }
 }
