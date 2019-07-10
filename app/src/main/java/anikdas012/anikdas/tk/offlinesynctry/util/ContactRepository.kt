16dbe7597bb48d2f1cf4603a32527d41c2993ac6
Anik Das <anikdas012@gmail.com>
2019-07-10 18:06:09 +0600

    Added primary constractor and a class property

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
@@ -1,4 +1,10 @@
 package anikdas012.anikdas.tk.offlinesynctry.util
 
-class ContactRepository {
+import androidx.lifecycle.LiveData
+import anikdas012.anikdas.tk.offlinesynctry.database.Contact
+import anikdas012.anikdas.tk.offlinesynctry.database.ContactDAO
+
+class ContactRepository(private val contactDao: ContactDAO) {
+
+    val allContacts: LiveData<List<Contact>> = contactDao.getAll()
 }
