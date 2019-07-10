a4f75c1d43074f1052453904b68c511005c1c641
Anik Das <anikdas012@gmail.com>
2019-07-10 19:00:39 +0600

    Added a static method to check network connection

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/AppUtil.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/AppUtil.kt
@@ -1,8 +1,18 @@
 package anikdas012.anikdas.tk.offlinesynctry.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
 class AppUtil {
     companion object {
         final val STATUS_SYNCED: Int = 0
         final val STATUS_UNSYNCED: Int = 1
+
+        fun isNetworkConnected(context: Context): Boolean {
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+            return activeNetwork?.isConnected ?: false
+        }
     }
 }
