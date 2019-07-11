b58ef2639739754d597ef5b760d6befea434695d
Anik Das <anikdas012@gmail.com>
2019-07-11 14:51:05 +0600

    Added a init block to initialize necessary properties

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
@@ -4,9 +4,16 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import anikdas012.anikdas.tk.offlinesynctry.database.Contact
+import anikdas012.anikdas.tk.offlinesynctry.database.ContactDatabase
 import anikdas012.anikdas.tk.offlinesynctry.util.ContactRepository
 
 class ContactViewModel(application: Application): AndroidViewModel(application) {
     private val repository: ContactRepository
     val allContacts: LiveData<List<Contact>>
+
+    init {
+        val contactDao = ContactDatabase.getDatabase(application).contactDao()
+        repository = ContactRepository(contactDao)
+        allContacts = repository.allContacts
+    }
 }
