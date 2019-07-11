92c1dfa804ffce0df9647ddd599548b8f0211891
Anik Das <anikdas012@gmail.com>
2019-07-11 14:50:17 +0600

    Declared two class property

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
@@ -2,6 +2,11 @@ package anikdas012.anikdas.tk.offlinesynctry.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import anikdas012.anikdas.tk.offlinesynctry.database.Contact
+import anikdas012.anikdas.tk.offlinesynctry.util.ContactRepository
 
 class ContactViewModel(application: Application): AndroidViewModel(application) {
+    private val repository: ContactRepository
+    val allContacts: LiveData<List<Contact>>
 }
