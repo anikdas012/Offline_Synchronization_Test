fe73b8ad3a965173ceb5b1a0ae03ac291c69e0e6
Anik Das <anikdas012@gmail.com>
2019-07-11 14:21:46 +0600

    Created a new class for viewmodel

---
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
@@ -0,0 +1,7 @@
+package anikdas012.anikdas.tk.offlinesynctry.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ContactViewModel(application: Application): AndroidViewModel(application) {
+}
