016f4de7a583d662ac5399aee05306a5d824aa7a
Anik Das <anikdas012@gmail.com>
2019-07-11 18:47:13 +0600

    Created a new coroutine with IO dispatcher to create new contact

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/viewmodel/ContactViewModel.kt
@@ -3,9 +3,12 @@ package anikdas012.anikdas.tk.offlinesynctry.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import anikdas012.anikdas.tk.offlinesynctry.database.Contact
 import anikdas012.anikdas.tk.offlinesynctry.database.ContactDatabase
 import anikdas012.anikdas.tk.offlinesynctry.util.ContactRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ContactViewModel(application: Application): AndroidViewModel(application) {
     private val repository: ContactRepository
@@ -15,5 +18,9 @@ class ContactViewModel(application: Application): AndroidViewModel(application) 
         val contactDao = ContactDatabase.getDatabase(application).contactDao()
         repository = ContactRepository(contactDao)
         allContacts = repository.allContacts
+    }
+
+    fun createContact(contact: Contact) = viewModelScope.launch(Dispatchers.IO) {
+        repository.createContact(contact)
     }
 }
