15230f0037740734c79b5a42db358ef5fe8b61df
Anik Das <anikdas012@gmail.com>
2019-07-15 17:47:03 +0600

    Created a inner class for view holder

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
@@ -1,7 +1,9 @@
 package anikdas012.anikdas.tk.offlinesynctry.adapter
 
 import android.content.Context
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactListAdapter internal constructor(context: Context): RecyclerView.Adapter<>{
+    inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {}
 }
