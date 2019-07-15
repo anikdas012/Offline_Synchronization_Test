a9ba808fd832dd8db6e3a0993b06846f2bec908e
Anik Das <anikdas012@gmail.com>
2019-07-15 17:44:49 +0600

    Created a new class for recycler view adapter and its taking context as a parameter

---
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
@@ -0,0 +1,7 @@
+package anikdas012.anikdas.tk.offlinesynctry.adapter
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactListAdapter internal constructor(context: Context): RecyclerView.Adapter<>{
+}
