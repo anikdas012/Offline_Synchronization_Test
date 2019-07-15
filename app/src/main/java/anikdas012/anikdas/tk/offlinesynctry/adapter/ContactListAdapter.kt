92d3c8a1afd796aa3873822142d0564b55f585b1
Anik Das <anikdas012@gmail.com>
2019-07-15 17:55:53 +0600

    Initalized class property for adapter

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
@@ -1,15 +1,20 @@
 package anikdas012.anikdas.tk.offlinesynctry.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import anikdas012.anikdas.tk.offlinesynctry.R
+import anikdas012.anikdas.tk.offlinesynctry.database.Contact
 
 class ContactListAdapter internal constructor(context: Context): RecyclerView.Adapter<ContactListAdapter.ContactViewHolder>(){
 
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var contacts = emptyList<Contact>()
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
