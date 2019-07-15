609ab43429bdf21a9f58ae444a06a39b2a1371a7
Anik Das <anikdas012@gmail.com>
2019-07-15 17:53:03 +0600

    Overriden necessary methods

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
@@ -2,6 +2,7 @@ package anikdas012.anikdas.tk.offlinesynctry.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +10,20 @@ import anikdas012.anikdas.tk.offlinesynctry.R
 import kotlinx.android.synthetic.main.list_items.view.*
 
 class ContactListAdapter internal constructor(context: Context): RecyclerView.Adapter<ContactListAdapter.ContactViewHolder>(){
+    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
     inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         val name: AppCompatTextView = itemView.findViewById(R.id.name_text)
         val number: AppCompatTextView = itemView.findViewById(R.id.number_text)
