026f2d9c8089af6896ce7df97e40adc1da594b56
Anik Das <anikdas012@gmail.com>
2019-07-15 17:50:12 +0600

    Initializing view components of recycler view

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/adapter/ContactListAdapter.kt
@@ -2,8 +2,16 @@ package anikdas012.anikdas.tk.offlinesynctry.adapter
 
 import android.content.Context
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
+import anikdas012.anikdas.tk.offlinesynctry.R
+import kotlinx.android.synthetic.main.list_items.view.*
 
 class ContactListAdapter internal constructor(context: Context): RecyclerView.Adapter<>{
-    inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {}
+    inner class ContactViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name: AppCompatTextView = itemView.findViewById(R.id.name_text)
+        val number: AppCompatTextView = itemView.findViewById(R.id.number_text)
+        val status: AppCompatImageView = itemView.findViewById(R.id.status_image)
+    }
 }
