34f964aef904a57fb21ad452077a2ec234a2f26a
Anik Das <anikdas012@gmail.com>
2019-07-15 18:21:15 +0600

    Initializing recycler view in the onCreate() method of MainActivity

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/MainActivity.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/MainActivity.kt
@@ -2,11 +2,18 @@ package anikdas012.anikdas.tk.offlinesynctry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import anikdas012.anikdas.tk.offlinesynctry.adapter.ContactListAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val adapter = ContactListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
