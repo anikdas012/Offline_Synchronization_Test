3a49a3953f6569405d79a6fc5d94cb18a979e6b9
Anik Das <anikdas012@gmail.com>
2019-07-17 19:04:08 +0600

    Created two static property to ApiClient class

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/network/retrofit/ApiClient.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/network/retrofit/ApiClient.kt
@@ -1,4 +1,12 @@
 package anikdas012.anikdas.tk.offlinesynctry.network.retrofit
 
+import retrofit2.Retrofit
+
 class ApiClient {
+
+    companion object {
+        @Volatile
+        private var retrofit: Retrofit? = null
+        private  val baseURL = "anikdas.tk/api"
+    }
 }
