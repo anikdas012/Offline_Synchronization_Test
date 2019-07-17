b36d7b3c2baaf3cd72843912bf8604d7a7628934
Anik Das <anikdas012@gmail.com>
2019-07-17 17:38:42 +0600

    Changed database table. Remove previous primary key and made number new primary key

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/Contact.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/Contact.kt
@@ -6,8 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "Contacts")
 data class Contact(
-    @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "Name") val name: String,
-    @ColumnInfo(name = "Phone_Number") val number: String,
+    @PrimaryKey @ColumnInfo(name = "Phone_Number") val number: String,
     val status: Int
 )
