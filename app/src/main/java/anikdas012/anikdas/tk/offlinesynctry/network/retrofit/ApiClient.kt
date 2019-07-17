a90016564d2c4944d9f15509d9c98b79698decc9
Anik Das <anikdas012@gmail.com>
2019-07-17 19:10:00 +0600

    Returning instance of retrofit in singleton pattern

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/network/retrofit/ApiClient.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/network/retrofit/ApiClient.kt
@@ -1,6 +1,7 @@
 package anikdas012.anikdas.tk.offlinesynctry.network.retrofit
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiClient {
 
@@ -8,5 +9,15 @@ class ApiClient {
         @Volatile
         private var retrofit: Retrofit? = null
         private  val baseURL = "anikdas.tk/api"
+
+        fun getInstance(): Retrofit {
+            if (retrofit == null) {
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit!!
+        }
     }
 }
