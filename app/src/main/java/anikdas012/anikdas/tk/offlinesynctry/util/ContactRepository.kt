422be2e6e33fc2bdd9d2bd921883cd4744178743
Anik Das <anikdas012@gmail.com>
2019-07-18 13:49:58 +0600

    Added comments

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
@@ -9,18 +9,30 @@ class ContactRepository(private val contactDao: ContactDAO) {
 
     val allContacts: LiveData<List<Contact>> = contactDao.getAll()
 
+    /**
+     * This method will add a new contact to
+     * database using a new thread
+     */
     @WorkerThread
     suspend fun createContact(contact: Contact) {
         contactDao.createNewContact(contact)
     }
 
 
+    /**
+     * This method will update an existing contact of
+     * database using a new thread
+     */
     @WorkerThread
     suspend fun updateContact(contact: Contact, syncStatus: Int) {
         contactDao.updateUnsynced(contact.number, syncStatus)
     }
 
 
+    /**
+     * This method will return all unsynced contacts
+     * from database
+     */
     fun getUnSyncedContacts(): List<Contact> {
         return contactDao.getAllUnsynced(AppUtil.STATUS_UNSYNCED)
     }
