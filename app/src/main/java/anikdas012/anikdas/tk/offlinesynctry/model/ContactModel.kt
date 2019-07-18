dc7d84049c30faaff5228f35032be676d22c3146
Anik Das <anikdas012@gmail.com>
2019-07-18 13:53:26 +0600

    Added comment

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/model/ContactModel.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/model/ContactModel.kt
@@ -3,6 +3,10 @@ package anikdas012.anikdas.tk.offlinesynctry.model
 import com.google.gson.annotations.SerializedName
 import org.jetbrains.annotations.Nullable
 
+/**
+ * This class is structured to send and receive
+ * responses from server
+ */
 data class ContactModel(
     @SerializedName("name") val name: String,
     @SerializedName("number") val number: String,
