a2886f5c21b06454155b4f7aacfb73144b265db3
Anik Das <anikdas012@gmail.com>
2019-07-18 13:40:36 +0600

    Added two methods to get and add new contacts

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/network/retrofit/ApiEndPoints.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/network/retrofit/ApiEndPoints.kt
@@ -1,4 +1,18 @@
 package anikdas012.anikdas.tk.offlinesynctry.network.retrofit
 
+import anikdas012.anikdas.tk.offlinesynctry.model.ContactModel
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
 interface ApiEndPoints {
+
+    @GET("/offlineSync")
+    fun getContacts(): Call<List<ContactModel>>
+
+    @Headers("Content-Type: application/json")
+    @POST("/offlineSync")
+    fun addContact(@Body contact: ContactModel): Call<ContactModel>
 }
