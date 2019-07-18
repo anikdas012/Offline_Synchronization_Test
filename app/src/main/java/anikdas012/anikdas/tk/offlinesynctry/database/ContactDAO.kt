0fc0ebc74dcec83e4b9a710a2e7f5b028da27aaa
Anik Das <anikdas012@gmail.com>
2019-07-18 13:57:11 +0600

    Added comments

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
@@ -8,18 +8,36 @@ import androidx.room.Query
 
 @Dao
 interface ContactDAO {
+    /**
+     * This method will add new contact to local database.
+     * If conflict happens it will abort operation
+     */
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun createNewContact(contact: Contact)
 
+    /**
+     * This method will delet all entries from local database
+     */
     @Query("DELETE FROM Contacts")
     fun deleteAll()
 
+    /**
+     * This method will return all contacts from local database
+     */
     @Query("SELECT * from Contacts")
     fun getAll(): LiveData<List<Contact>>
 
+    /**
+     * This method will return all unsynced contacts from
+     * local database
+     */
     @Query("SELECT * from Contacts WHERE status = :syncStatus")
     fun getAllUnsynced(syncStatus: Int): List<Contact>
 
+    /**
+     * This method will update sysnc status of all
+     * unsynced contacts in local database
+     */
     @Query("UPDATE Contacts SET status = :syncStatus WHERE Phone_Number = :phoneNumber")
     suspend fun updateUnsynced(phoneNumber: String, syncStatus: Int)
 }
