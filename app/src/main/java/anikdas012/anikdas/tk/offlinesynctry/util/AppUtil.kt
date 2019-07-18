f6860034d78732aba1ccf9f1064444cc2ea1cc6c
Anik Das <anikdas012@gmail.com>
2019-07-18 13:54:19 +0600

    Added comment

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/AppUtil.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/AppUtil.kt
@@ -9,6 +9,10 @@ class AppUtil {
         final val STATUS_SYNCED: Int = 0
         final val STATUS_UNSYNCED: Int = 1
 
+        /**
+         * This method will return current
+         * network condition
+         */
         fun isNetworkConnected(context: Context): Boolean {
             val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
             val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
