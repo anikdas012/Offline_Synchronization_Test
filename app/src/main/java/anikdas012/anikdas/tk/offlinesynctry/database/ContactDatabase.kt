3433e6cd065c3766e0644fe5ea9cb43ceb4d6ea0
Anik Das <anikdas012@gmail.com>
2019-07-21 15:50:13 +0600

    Replaced arrayOf() with []

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDatabase.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-@Database(entities = arrayOf(Contact::class), version = 1)
+@Database(entities = [Contact::class], version = 1)
 abstract class ContactDatabase: RoomDatabase() {
 
     abstract fun contactDao(): ContactDAO
