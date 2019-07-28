6d0bd2802ab867897c216ce46f85965edf5e1e20
Anik Das <anikdas012@gmail.com>
2019-07-28 18:55:02 +0600

    Extended from Worker class

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/synchronization/SyncWorker.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/synchronization/SyncWorker.kt
@@ -1,11 +1,12 @@
 package anikdas012.anikdas.tk.offlinesynctry.synchronization
 
 import android.content.Context
+import androidx.work.Worker
 import androidx.work.WorkerParameters
 
 /**
  * Created by "Anik Das" on 7/28/2019
  * Developer email: "anikdas012@gmail.com"
  */
-class SyncWorker(context: Context, params: WorkerParameters) {
+class SyncWorker(context: Context, params: WorkerParameters): Worker(context, params) {
 }
