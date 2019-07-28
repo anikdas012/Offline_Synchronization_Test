9a8a9c72d68d5d29655602401ff62435e177ff51
Anik Das <anikdas012@gmail.com>
2019-07-28 18:53:44 +0600

    Creatd a new worker class for syncing data between server and local db

---
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/synchronization/SyncWorker.kt
@@ -0,0 +1,11 @@
+package anikdas012.anikdas.tk.offlinesynctry.synchronization
+
+import android.content.Context
+import androidx.work.WorkerParameters
+
+/**
+ * Created by "Anik Das" on 7/28/2019
+ * Developer email: "anikdas012@gmail.com"
+ */
+class SyncWorker(context: Context, params: WorkerParameters) {
+}
