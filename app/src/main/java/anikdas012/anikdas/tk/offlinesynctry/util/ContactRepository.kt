15c5bd5ecfbd9bab7000f5aef0f684b923cc25b2
Anik Das <anikdas012@gmail.com>
2019-11-06 15:43:28 +0600

    Updated comments

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/util/ContactRepository.kt
@@ -12,9 +12,12 @@ class ContactRepository(private val contactDao: ContactDAO) {
 
     val allContacts: LiveData<List<Contact>> = contactDao.getAll()
 
+
     /**
      * This method will add a new contact to
      * database using a new thread
+     *
+     * @param contact: {@link: anikdas012.anikdas.tk.offlinesynctry.database.Contact} object
      */
     @WorkerThread
     suspend fun createContact(contact: Contact) {
@@ -26,6 +29,9 @@ class ContactRepository(private val contactDao: ContactDAO) {
     /**
      * This method will update an existing contact of
      * database using a new thread
+     *
+     * @param number: Phone number of the contact
+     * @param syncStatus: Synchronization status
      */
     @WorkerThread
     suspend fun updateContact(number: String, syncStatus: Int) {
@@ -37,6 +43,8 @@ class ContactRepository(private val contactDao: ContactDAO) {
     /**
      * This method will return all unsynced contacts
      * from database
+     *
+     * @return List of unsynchronized contacts
      */
     fun getUnSyncedContacts(): List<Contact> {
         Log.d(LOG_TAG, "getUnSyncedContacts")
@@ -47,6 +55,9 @@ class ContactRepository(private val contactDao: ContactDAO) {
     /**
      * This method will return a specific contact from
      * database if exists
+     *
+     * @param number: Phone number of the contact
+     * @return contact with specific phone number
      */
     fun getContactFromDatabase(number: String): Contact? {
         Log.d(LOG_TAG, "getContactFromDatabase")
