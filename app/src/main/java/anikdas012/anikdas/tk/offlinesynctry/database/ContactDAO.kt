edd69fa92e558880bb0faae0744f6b87e5d61cf0
Anik Das <anikdas012@gmail.com>
2019-11-07 14:25:24 +0600

    Updated comments

--- app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
+++ app/src/main/java/anikdas012/anikdas/tk/offlinesynctry/database/ContactDAO.kt
@@ -11,6 +11,8 @@ interface ContactDAO {
     /**
      * This method will add new contact to local database.
      * If conflict happens it will abort operation
+     *
+     * @param contact: Contact object
      */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun createNewContact(contact: Contact)
@@ -23,6 +25,8 @@ interface ContactDAO {
 
     /**
      * This method will return all contacts from local database
+     *
+     * @return Observable list of contact objects
      */
     @Query("SELECT * from Contacts")
     fun getAll(): LiveData<List<Contact>>
@@ -30,6 +34,8 @@ interface ContactDAO {
     /**
      * This method will return all unsynced contacts from
      * local database
+     *
+     * @return List of contact objects
      */
     @Query("SELECT * from Contacts WHERE status = :syncStatus")
     fun getAllUnsynced(syncStatus: Int): List<Contact>
@@ -37,6 +43,9 @@ interface ContactDAO {
     /**
      * This method will update sysnc status of all
      * unsynced contacts in local database
+     *
+     * @param phoneNumber: Phone number of a contact
+     * @param syncStatus: Synchronization status
      */
     @Query("UPDATE Contacts SET status = :syncStatus WHERE Phone_Number = :phoneNumber")
     suspend fun updateUnsynced(phoneNumber: String, syncStatus: Int)
@@ -44,6 +53,9 @@ interface ContactDAO {
     /**
      * This method will return a contact from database using phone number
      * if exists
+     *
+     * @param phoneNumber: Phone number of a contact
+     * @return Contact object or null
      */
     @Query("SELECT * from Contacts WHERE Phone_Number = :phoneNumber")
     fun getContact(phoneNumber: String): Contact?
